248dd581b1fd09432fa673880a1da287f19f4d52
Michal Svačina <msvacina@outlook.com>
2021-02-21 11:40:29 +0100

    Implemented Emploeyees db table check and creation

--- api/employees.js
+++ api/employees.js
@@ -9,18 +9,46 @@ const client = new Client({
     password: 'Nodeheslo',
     port: 5432,
 });
+const tableName = 'employees'; 
 
-try {
-    client.connect();
-} catch (error) {
-    console.log(error);
-}
+client.connect(err => {
+    if (err) {
+        console.error('Cannot connect to PostgreSQL', err.stack);
+    } else {
+        console.log('Connected to PostgreSQL');
 
-router.get('/', async (req,res,next) => {
+        // Check if table is created in DB. If not create it.
+        var select = 'SELECT * FROM pg_catalog.pg_tables where tablename = $1';
+        var values = [ tableName ];
+        
+        console.log(`Check existence for table ${tableName}`);
+        client.query(select, values, (error, result) => {
+            if (result.rowCount == 0)
+            {
+                // Create table.
+                console.log(`Table ${tableName} does not exist`);
+                client.query('CREATE SEQUENCE IF NOT EXISTS employeeId;');
+                client.query('CREATE TABLE employees (id integer NOT NULL DEFAULT nextval(\'employeeId\'), name varchar(64) NOT NULL, position varchar(32) NOT NULL,  salary integer NOT NULL, managerId integer, PRIMARY KEY(id));', (error2, result2) => {
+                    if (error2 == null) {
+                        console.log(`Table ${tableName} created successfully`);
+                    }
+                });
+            } else {
+                console.debug(`Table ${tableName} already exists`);
+            }
+        });
+    }
+});
+
+router.get('/', async (req, res, next) => {
     try {
-        client.query('SELECT NOW()', (err, result) => {
-            console.log(result)
-            
+        client.query('SELECT NOW()', (error, result) => {
+            console.debug(result)
+            if (error) {
+                console.error('Connection error', err.stack);
+                next(error)
+            }
+
             var postgresStatus;
             if (result.rowCount == 1) {
                 postgresStatus = true;
@@ -34,7 +62,7 @@ router.get('/', async (req,res,next) => {
             });
         });
     } catch (error) {
-        next (error);
+        next(error);
     }
 });
 
